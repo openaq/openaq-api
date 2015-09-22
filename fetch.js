9493257c6a24abbb4618df98c281b9efce5bb77d
Joe Flasher <joseph.flasher@gmail.com>
2015-09-21 20:41:55 -0400

    Adding a --dryrun and --sources options to fetch process
    Details can be seen by running `node fetch.js -h`

--- fetch.js
+++ fetch.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// Set up command line arguments
+var argv = require('yargs')
+  .usage('Usage: $0 --dryrun --source \'Beijing US Embassy\'')
+  .boolean('dryrun')
+  .describe('dryrun', 'Run the fetch process but do not attempt to save to the database and instead print to console, useful for testing.')
+  .alias('d', 'dryrun')
+  .describe('source', 'Run the fetch process with only the defined source using source name.')
+  .alias('s', 'source')
+  .nargs('source', 1)
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
 var async = require('async');
 var _ = require('lodash');
 var MongoClient = require('mongodb').MongoClient;
@@ -12,8 +25,12 @@ var sources = require('./sources');
 var dbURL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/openAQ';
 var measurementsCollection;
 
-// Flatten the sources into a single array
+// Flatten the sources into a single array, taking into account sources argument
 sources = _.chain(sources).values().flatten().value();
+if (argv.source) {
+  sources = _.find(sources, { name: argv.source });
+  sources = [sources];
+}
 
 var findAdapter = function (name) {
   return _.find(adapters, function (a) {
@@ -33,7 +50,10 @@ var getAndSaveData = function (source) {
     adapter.fetchData(source, function (err, data) {
       // If we have an error, send an email to the contacts and stop
       if (err) {
-        mailer.sendFailureEmail(source.contacts, source.name, err);
+        // Don't send an email if it's a dry run
+        if (!argv.dryrun) {
+          mailer.sendFailureEmail(source.contacts, source.name, err);
+        }
         err.source = source.name;
         return done(null, err);
       }
@@ -44,7 +64,10 @@ var getAndSaveData = function (source) {
       // If the data format is invalid, let the contacts know
       if (!isValid) {
         var error = {message: 'Adapter returned invalid results.', source: source.name};
-        mailer.sendFailureEmail(source.contacts, source.name, error);
+        // Don't send an email if it's a dry run
+        if (!argv.dryrun) {
+          mailer.sendFailureEmail(source.contacts, source.name, error);
+        }
         return done(null, error);
       }
 
@@ -60,27 +83,46 @@ var getAndSaveData = function (source) {
           message: 'New measurements inserted for ' + source.name + ': 0',
           source: source.name
         };
+        // A little hacky to signify a dry run
+        if (argv.dryrun) {
+          msg.message = '[Dry run] ' + msg.message;
+        }
         return done(null, msg);
       }
 
-      var bulk = measurementsCollection.initializeUnorderedBulkOp();
+      // We can cut out some of the db related tasks if this is a dry run
+      if (!argv.dryrun) {
+        var bulk = measurementsCollection.initializeUnorderedBulkOp();
+      }
       _.forEach(data.measurements, function (m) {
         m.location = m.location || data.name; // use existing location if it exists
-        m.country = source.country;
+        m.country = m.country || source.country;
         m.city = m.city || source.city; // use city from measurement, otherwise default to source
         m.sourceName = source.name;
-        bulk.insert(m);
-      });
-      bulk.execute(function (err, result) {
-        if (err) {
-          // No need to log this out for now
+        if (argv.dryrun) {
+          console.info(m);
+        } else {
+          bulk.insert(m);
         }
-        var msg = {
-          message: 'New measurements inserted for ' + source.name + ': ' + result.nInserted,
+      });
+      if (argv.dryrun) {
+        msg = {
+          message: '[Dry run] New measurements inserted for ' + source.name + ': ' + data.measurements.length,
           source: source.name
         };
         done(null, msg);
-      });
+      } else {
+        bulk.execute(function (err, result) {
+          if (err) {
+            // No need to log this out for now
+          }
+          var msg = {
+            message: 'New measurements inserted for ' + source.name + ': ' + result.nInserted,
+            source: source.name
+          };
+          done(null, msg);
+        });
+      }
     });
   };
 };
@@ -89,40 +131,55 @@ var tasks = _.map(sources, function (source) {
   return getAndSaveData(source);
 });
 
-MongoClient.connect(dbURL, function (err, db) {
-  if (err) {
-    return console.error(err);
-  }
-  console.info('Connected to database.');
-
-  // Get collection and ensure indexes
-  measurementsCollection = db.collection('measurements');
-  async.parallel([
-    function (done) {
-      measurementsCollection.createIndex({ location: 1, parameter: 1, date: 1 }, { unique: true }, function (err) {
-        done(err);
-      });
-    },
-    function (done) {
-      measurementsCollection.createIndex({ city: 1 }, { background: true }, function (err) {
-        done(err);
-      });
-    }
-  ], function (err, results) {
+var runTasks = function (db) {
+  async.parallel(tasks, function (err, results) {
     if (err) {
+      console.error(err);
+    } else {
+      if (!argv.dryrun) {
+        console.info('All data grabbed and saved.');
+      }
+      console.info(results);
+    }
+
+    // Close the database if it exists
+    if (db) {
       db.close();
+    }
+  });
+};
+
+// Branch here depending on whether this is a dryrun or not
+if (argv.dryrun) {
+  console.info('--- Dry run for Testing, nothing is saved to the database. ---');
+  runTasks();
+} else {
+  MongoClient.connect(dbURL, function (err, db) {
+    if (err) {
       return console.error(err);
     }
-    console.info('Indexes created and database ready to go.');
-    async.parallel(tasks, function (err, results) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.info('All data grabbed and saved.');
-        console.info(results);
-      }
+    console.info('Connected to database.');
 
-      db.close();
+    // Get collection and ensure indexes
+    measurementsCollection = db.collection('measurements');
+    async.parallel([
+      function (done) {
+        measurementsCollection.createIndex({ location: 1, parameter: 1, date: 1 }, { unique: true }, function (err) {
+          done(err);
+        });
+      },
+      function (done) {
+        measurementsCollection.createIndex({ city: 1 }, { background: true }, function (err) {
+          done(err);
+        });
+      }
+    ], function (err, results) {
+      if (err) {
+        db.close();
+        return console.error(err);
+      }
+      console.info('Indexes created and database ready to go.');
+      runTasks(db);
     });
   });
-});
+}
