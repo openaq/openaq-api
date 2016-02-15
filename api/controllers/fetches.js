826225fafdd7271fb63b7d6378632b5547f410b5
Dolugen <dolugen@gmail.com>
2016-02-15 23:13:01 +0800

    Order fetches by ID #188

--- api/controllers/fetches.js
+++ api/controllers/fetches.js
@@ -33,7 +33,7 @@ module.exports.query = function (query, page, limit, cb) {
     let resultsQuery = db
                         .select(['time_started', 'time_ended', 'count', 'results'])
                         .from('fetches')
-                        .orderBy('time_started', 'desc')
+                        .orderBy('id', 'desc')
                         .limit(limit).offset(skip);
     // Build on base query
     resultsQuery = utils.buildSQLQuery(resultsQuery, payload, operators, betweens, nulls, notNulls);
