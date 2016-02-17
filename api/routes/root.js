d7834a2198a49ecd5c45c402f7656e486a09a6d2
Dolugen <dolugen@gmail.com>
2016-02-17 13:06:34 +0800

    Pass the style check #183

--- api/routes/root.js
+++ api/routes/root.js
@@ -1,20 +1,20 @@
 'use strict';
 
 // List all sub-level endpoints
-const rootRouteHandler = function(request, reply) {
-    var table = request.server.table(request.server.info.host)[0].table;
-    var endpoints = [];
-    table.forEach(function(route) {
-        var path = route.public.path;
-        if (path.startsWith(request.path) && path != request.path) {
-            endpoints.push({
-                'method': route.public.method.toUpperCase(),
-                'path': request.server.info.uri + path
-            });
-        }
-    });
-    return reply(endpoints)
-}
+const rootRouteHandler = function (request, reply) {
+  var table = request.server.table(request.server.info.host)[0].table;
+  var endpoints = [];
+  table.forEach(function (route) {
+    var path = route.public.path;
+    if (path.startsWith(request.path) && path !== request.path) {
+      endpoints.push({
+        'method': route.public.method.toUpperCase(),
+        'path': request.server.info.uri + path
+      });
+    }
+  });
+  return reply(endpoints);
+};
 
 module.exports = [
   {
