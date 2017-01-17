54b44b05681ed448baca95adf1f49919dbdf5b05
jflasher <joseph.flasher@gmail.com>
2017-01-17 18:28:25 -0500

    Raising pool limit for psql

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,10 @@ module.exports = {
     password: process.env.PSQL_PASSWORD || '',
     database: process.env.PSQL_DATABASE || 'openaq-local'
   },
+  pool: {
+    min: process.env.PSQL_POOL_MIN || 2,
+    max: process.env.PSQL_POOL_MAX || 10
+  },
   migrations: {
     tableName: 'migrations'
   }
