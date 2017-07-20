aad60de4713a6b57b66b59fcb91c1cab6ad5ac05
Dolugen Buuralda <dolugen@gmail.com>
2017-07-20 12:16:32 +0800

    Add averagingPeriod to latest endpoint

--- api/routes/latest.js
+++ api/routes/latest.js
@@ -36,21 +36,33 @@ import { log } from '../services/logger';
  *              "value": "7.8",
  *              "lastUpdated": "2015-07-24T11:30:00.000Z",
  *              "unit": "µg/m3",
- *              "sourceName": "Punjabi Bagh"
+ *              "sourceName": "Punjabi Bagh",
+ *              "averagingPeriod": {
+ *                "unit": "hours",
+ *                "value": 0.25
+ *              }
  *             },
  *             {
  *               "parameter": "co",
  *               "value": 1.3,
  *               "lastUpdated": "2015-08-18T23:30:00.000Z",
  *               "unit": "mg/m3",
- *               "sourceName": "CPCB"
+ *               "sourceName": "CPCB",
+ *               "averagingPeriod": {
+ *                 "unit": "hours",
+ *                 "value": 0.25
+ *               }
  *             },
  *             {
  *               "parameter": "pm25",
  *               "value": 79,
  *               "lastUpdated": "2015-10-02T21:45:00.000Z",
  *               "unit": "µg/m3",
- *               "sourceName": "CPCB"
+ *               "sourceName": "CPCB",
+ *               "averagingPeriod": {
+ *                 "unit": "hours",
+ *                 "value": 0.25
+ *               }
  *             }
  *           ]
  *             ...
