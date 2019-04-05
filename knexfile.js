6d7ad6eccefe08fd0e5e85d0ff33da0cd64bed13
Vitor George <vitor.george@gmail.com>
2019-04-05 18:41:29 +0100

    Fix knex pool min/max value types.

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,17 @@
 require('babel-register');
 const config = require('config');
-module.exports = config.get('knex');
+
+// Get a mutable object from config
+const knexConfig = JSON.parse(JSON.stringify(config.get('knex')));
+
+// Fix pool min/max types when using environment variables
+if (knexConfig.pool) {
+  if (typeof knexConfig.pool.min !== 'undefined') {
+    knexConfig.pool.min = parseInt(knexConfig.pool.min);
+  }
+  if (typeof knexConfig.pool.max !== 'undefined') {
+    knexConfig.pool.max = parseInt(knexConfig.pool.max);
+  }
+}
+
+module.exports = knexConfig;
