2bd118653b98807eed19194922ecb3bc813dbca4
Vitor George <vitor.george@gmail.com>
2019-05-07 11:58:45 +0100

    Refactor /cities endpoint.

--- test/test-cities.js
+++ test/test-cities.js
@@ -1,10 +1,28 @@
 import { expect } from 'chai';
 import request from 'request';
 import { orderBy } from 'lodash';
+import { db } from '../api/services/db';
+import { readJson } from 'fs-extra';
+import path from 'path';
+import { upsertCities } from '../api/services/athena-sync';
 
-/* global apiUrl */
+/* global apiUrl,fixturesPath */
 
-describe('/cities', function () {
+describe.only('/cities', function () {
+  // Populate cities table before testing.
+  before(async () => {
+    // Clear table
+    await db.delete().from('cities');
+
+    // Get 2018 cities from Athena
+    const athenaGetCities2016 = await readJson(
+      path.join(fixturesPath, 'athena-query-results/get-cities-2018.json')
+    );
+
+    // Upsert cities
+    await upsertCities(athenaGetCities2016);
+  });
+
   it('should return properly', function (done) {
     request(apiUrl + 'cities', function (err, response, body) {
       if (err) {
@@ -12,10 +30,11 @@ describe('/cities', function () {
       }
 
       var res = JSON.parse(body);
-      expect(res.results.length).to.equal(39);
+      expect(res.results.length).to.equal(100);
       done();
     });
   });
+
   it('has a meta block', function (done) {
     request(apiUrl + 'cities', function (err, response, body) {
       if (err) {
@@ -29,12 +48,13 @@ describe('/cities', function () {
         website: 'https://docs.openaq.org/',
         page: 1,
         limit: 100,
-        found: 39
+        found: 388
       };
       expect(res.meta).to.deep.equal(testMeta);
       done();
     });
   });
+
   it('has pages', function (done) {
     request(apiUrl + 'cities?limit=1', function (err, response, body) {
       if (err) {
