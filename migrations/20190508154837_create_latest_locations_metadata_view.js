94f9e06a9cf72bcbb977d601b76e6a98234ef542
Daniel da Silva <daniel@developmentseed.org>
2019-05-09 14:55:22 +0100

    Add table and view for location metadata

---
+++ migrations/20190508154837_create_latest_locations_metadata_view.js
@@ -0,0 +1,29 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.raw(`
+    CREATE VIEW latest_locations_metadata AS
+    SELECT
+      locations_metadata.id,
+      locations_metadata."locationId",
+      locations_metadata."userId",
+      locations_metadata.data,
+      lm."createdAt",
+      lm."updatedAt",
+      lm.version
+    FROM locations_metadata
+    INNER JOIN (
+      SELECT
+        "locationId",
+        max("createdAt") as "updatedAt",
+        min("createdAt") as "createdAt",
+        COUNT("locationId") as version
+      FROM locations_metadata
+      GROUP BY "locationId"
+    ) lm ON locations_metadata."locationId" = lm."locationId" AND locations_metadata."createdAt" = lm."updatedAt"
+  `);
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.raw(`
+    DROP VIEW latest_locations_metadata IF EXISTS
+  `);
+};
