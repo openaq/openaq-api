d6e8bba708ef03418023f7f3841c173e40ff235e
Daniel da Silva <daniel@developmentseed.org>
2019-05-10 15:26:27 +0100

    Remove foreign key from locations metadata table

--- migrations/20190508140950_create_location_metadata_table.js
+++ migrations/20190508140950_create_location_metadata_table.js
@@ -2,8 +2,6 @@ exports.up = function (knex) {
   return knex.schema.createTable('locations_metadata', function (table) {
     table.increments('id').primary();
     table.string('locationId').notNullable();
-    table.foreign('locationId')
-      .references('locations.id');
     table.timestamp('createdAt').defaultTo(knex.fn.now());
     table.string('userId');
     table.jsonb('data');
