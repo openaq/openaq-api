e69e5044e03fd7c99fba72c14ae8245ab9f082f8
sruti <sruti@openaq.org>
2020-06-30 06:29:03 -0700

    Update averaging endpoint docs and functionality

--- api/controllers/averages.js
+++ api/controllers/averages.js
@@ -1,9 +1,8 @@
 'use strict';
 
-import { forEach, has, omit, isEmpty, intersection, pick } from 'lodash';
+import { has, omit, isEmpty, pick } from 'lodash';
 import { db } from '../services/db';
 import {
-    buildLocationsWhere,
     buildSQLQuery,
     queryFromParameters
 } from '../../lib/utils';
@@ -14,79 +13,79 @@ import moment from 'moment';
  *
  * @param {Object} query - Payload contains query parameters and their values
  */
-const queryCountViaLocations = async function (query) {
-    let totalCount;
+// const queryCountViaLocations = async function (query) {
+//     let totalCount;
 
-    // Flag if query has "parameter" property defined.
-    const countByParameters = query.parameter && query.parameter.length > 0;
+//     // Flag if query has "parameter" property defined.
+//     const countByParameters = query.parameter && query.parameter.length > 0;
 
-    // Create query
-    let counts = await db
-        .select(countByParameters ? 'countsByMeasurement' : 'count')
-        .from('locations')
-        .where(buildLocationsWhere(query));
+//     // Create query
+//     let counts = await db
+//         .select(countByParameters ? 'countsByMeasurement' : 'count')
+//         .from('locations')
+//         .where(buildLocationsWhere(query));
 
-    // If querying by parameter, break down count per measurement
-    if (countByParameters) {
-        // Convert parameters to array, if string
-        const queryParameters = [].concat(query.parameter);
+//     // If querying by parameter, break down count per measurement
+//     if (countByParameters) {
+//         // Convert parameters to array, if string
+//         const queryParameters = [].concat(query.parameter);
 
-        // Add values by parameter
-        totalCount = counts.reduce((count, i) => {
-            return (
-                count +
-                i.countsByMeasurement.reduce((pCount, p) => {
-                    // Filter by queried parameters
-                    return pCount + (queryParameters.includes(p.parameter) ? p.count : 0);
-                }, 0)
-            );
-        }, 0);
-    } else {
-        // If no parameters were passed, return total count
-        totalCount = counts.reduce((count, i) => {
-            return count + i.count;
-        }, 0);
-    }
+//         // Add values by parameter
+//         totalCount = counts.reduce((count, i) => {
+//             return (
+//                 count +
+//                 i.countsByMeasurement.reduce((pCount, p) => {
+//                     // Filter by queried parameters
+//                     return pCount + (queryParameters.includes(p.parameter) ? p.count : 0);
+//                 }, 0)
+//             );
+//         }, 0);
+//     } else {
+//         // If no parameters were passed, return total count
+//         totalCount = counts.reduce((count, i) => {
+//             return count + i.count;
+//         }, 0);
+//     }
 
-    return totalCount;
-};
+//     return totalCount;
+// };
 
 /**
  * Query row count via measurements table.
  *
  * @param {Object} query - Payload contains query parameters and their values
  */
-const queryCountViaMeasurements = async function (query) {
-    // Omit query parameters that doesn't affect count
-    query = omit(query, ['sort', 'limit', 'page', 'order_by', 'include_fields']);
+// const queryCountViaMeasurements = async function (query) {
+//     // Omit query parameters that doesn't affect count
+//     query = omit(query, ['sort', 'limit', 'page', 'order_by', 'include_fields']);
 
-    // Parse request as database query
-    let {
-        payload,
-        operators,
-        betweens,
-        nulls,
-        notNulls,
-        geo
-    } = queryFromParameters(query);
+//     // Parse request as database query
+//     let {
+//         payload,
+//         operators,
+//         betweens,
+//         nulls,
+//         notNulls,
+//         geo
+//     } = queryFromParameters(query);
 
-    // Get base db query
-    let countQuery = db.count('location').from('measurements');
+//     // Get base db query
+//     let countQuery = db.count('location').from('measurements');
 
-    // Apply parameters
-    countQuery = buildSQLQuery(
-        countQuery,
-        payload,
-        operators,
-        betweens,
-        nulls,
-        notNulls,
-        geo
-    );
+//     // Apply parameters
+//     countQuery = buildSQLQuery(
+//         countQuery,
+//         payload,
+//         operators,
+//         betweens,
+//         nulls,
+//         notNulls,
+//         geo
+//     );
 
-    // Return count
-    return Number((await countQuery)[0].count);
-};
+//     // Return count
+//     return Number((await countQuery)[0].count);
+// };
 
 /**
  * Query Measurements. Implements all protocols supported by /measurements endpoint
@@ -103,8 +102,8 @@ module.exports.query = async function (query, page, limit) {
      * When query filters by values or date, the count is fetched via
      * measurements table, which do not include full history.
      */
-    ////DON'T NEED FOLLOWING I'M PRETTY SURE
-    let count;
+    // // DON'T NEED FOLLOWING I'M PRETTY SURE
+  let count;
     // if (
     //     intersection(Object.keys(query), [
     //         'value_from',
@@ -119,7 +118,7 @@ module.exports.query = async function (query, page, limit) {
     // }
 
     // Turn the payload into something we can use with psql
-    let {
+  let {
         payload,
         operators,
         betweens,
@@ -131,17 +130,16 @@ module.exports.query = async function (query, page, limit) {
     //
     // Handle include_fields cases
     //
-    var projection = [
-        'country',
-        'city',
-        'location',
-        'coordinates',
-        'parameter',
-        'date',
-        'average',
-        'measurement_count',
-        'percent_reporting'
-    ];
+  var projection = [
+    'country',
+    'city',
+    'location',
+    'coordinates',
+    'parameter',
+    'date',
+    'average',
+    'measurement_count'
+  ];
 
     // if (has(payload, 'include_fields')) {
     //     // Turn into an array and add to projection
@@ -157,68 +155,66 @@ module.exports.query = async function (query, page, limit) {
     //
     // Handle custom sorts, starting with default of most recent measurements first.
     //
-    const defaultSort = [{ column: 'date_utc', direction: 'desc' }];
-    let sort = [];
-    // if (has(payload, 'order_by')) {
-    //     payload.order_by = [].concat(payload.order_by);
-    //     if (has(payload, 'sort')) {
-    //         payload.sort = [].concat(payload.sort);
-    //     }
-    //     payload.order_by.forEach((column, i, columns) => {
-    //         // skip unknown columns
-    //         if (projection.indexOf(column) === -1) {
-    //             return;
-    //         }
-
-    //         // Catch case where order_by is provided as 'date'
-    //         if (column === 'date') {
-    //             column = 'date_utc';
-    //         }
-    //         if (column === 'sourceName') {
-    //             column = 'source_name';
-    //         }
-    //         let direction;
-    //         try {
-    //             direction = payload.sort[i];
-    //         } catch (err) {
-    //             direction = 'asc';
-    //         }
-    //         sort.push({
-    //             column: column,
-    //             direction: direction
-    //         });
-    //     });
-
-    //     // sanitized payload
-    //     payload = omit(payload, 'order_by');
-    // }
-
-    // if (has(payload, 'sort')) {
-    //     // sanitized payload
-    //     payload = omit(payload, 'sort');
-    // }
-
-    if (isEmpty(sort)) {
-        sort = defaultSort;
+  const defaultSort = [{ column: 'date_utc', direction: 'desc' }];
+  let sort = [];
+  if (has(payload, 'order_by')) {
+    payload.order_by = [].concat(payload.order_by);
+    if (has(payload, 'sort')) {
+      payload.sort = [].concat(payload.sort);
     }
+    payload.order_by.forEach((column, i, columns) => {
+            // skip unknown columns
+      if (projection.indexOf(column) === -1) {
+        return;
+      }
+
+            // Catch case where order_by is provided as 'date'
+      if (column === 'date') {
+        column = 'date_utc';
+      }
+
+      let direction;
+      try {
+        direction = payload.sort[i];
+      } catch (err) {
+        direction = 'asc';
+      }
+      sort.push({
+        column: column,
+        direction: direction
+      });
+    });
+
+        // sanitized payload
+    payload = omit(payload, 'order_by');
+  }
+
+  if (has(payload, 'sort')) {
+        // sanitized payload
+    payload = omit(payload, 'sort');
+  }
+
+  if (isEmpty(sort)) {
+    sort = defaultSort;
+  }
 
     //
     // Apply paging
     //
-    var skip = limit * (page - 1);
+  var skip = limit * (page - 1);
 
     // Base query
-    let resultsQuery = db
+  let resultsQuery = db
         .select('*')
         .from('averages')
         .limit(limit)
         .offset(skip);
-    sort.forEach(s => {
-        resultsQuery = resultsQuery.orderBy(s.column, s.direction);
-    });
+  sort.forEach(s => {
+    resultsQuery = resultsQuery.orderBy(s.column, s.direction);
+  });
 
     // Build on base query
-    resultsQuery = buildSQLQuery(
+  resultsQuery = buildSQLQuery(
         resultsQuery,
         payload,
         operators,
@@ -229,41 +225,41 @@ module.exports.query = async function (query, page, limit) {
     );
 
     // Run the query
-    return resultsQuery.then(records => {
+  return resultsQuery.then(records => {
         // Move data obj to top level and handle projections
-        records = records.map(r => {
-            const coordinates = {
-                longitude: Number(r.lon),
-                latitude: Number(r.lat)
-            }
-            r.coordinates = coordinates
-            r.date = formatDate(payload.temporal, r.date_utc) //in DB, is local, should change to utc for now for simplicity
-            return omit(pick(r, projection), pickSpatialParams(payload.spatial));
-        });
-        return { records, count };
+    records = records.map(r => {
+      const coordinates = {
+        longitude: Number(r.lon),
+        latitude: Number(r.lat)
+      };
+      r.coordinates = coordinates;
+      r.date = formatDate(payload.temporal, r.date_utc); // in DB, is local, should change to utc for now for simplicity
+      return omit(pick(r, projection), pickSpatialParams(payload.spatial));
     });
+    return { records, count };
+  });
 };
 
-function formatDate(temporal, date) {
-    switch (temporal) {
-        case 'day':
-            return moment(date).format('YYYY-MM-DD')
-        case 'month':
-            return moment(date).format('YYYY-MM')
-        case 'year':
-            return moment(date).format('YYYY')
-        default:
-            return date;
-    }
+function formatDate (temporal, date) {
+  switch (temporal) {
+    case 'day':
+      return moment(date).format('YYYY-MM-DD');
+    case 'month':
+      return moment(date).format('YYYY-MM');
+    case 'year':
+      return moment(date).format('YYYY');
+    default:
+      return date;
+  }
 }
 
-function pickSpatialParams(spatial) {
-    switch (spatial) {
-        case 'city':
-            return ['location', 'coordinates']
-        case 'country':
-            return ['location', 'coordinates', 'city']
-        default:
-            return [];
-    }
+function pickSpatialParams (spatial) {
+  switch (spatial) {
+    case 'city':
+      return ['location', 'coordinates'];
+    case 'country':
+      return ['location', 'coordinates', 'city'];
+    default:
+      return [];
+  }
 }
--- api/routes/averages.js
+++ api/routes/averages.js
@@ -2,81 +2,58 @@
 
 import config from 'config';
 import _ from 'lodash';
-import { log } from '../services/logger';
 import { lonLatRegex } from '../../lib/utils';
 import Boom from 'boom';
-import csv from 'csv-stringify';
 import Joi from 'joi';
 import m from '../controllers/averages.js';
 
 const maxRequestLimit = config.get('maxRequestLimit');
 const defaultRequestLimit = config.get('defaultRequestLimit');
 
-//TODO: UPDATE API DESCRIPTION BELOW
+// TODO: ADD IN AVERAGING METHODOLOGY BELOW
 /**
  * @api {get} /averages GET
- * @apiGroup Measurements
- * @apiDescription Provides data about individual measurements
+ * @apiGroup Averages
+ * @apiDescription (BETA) Provides averages across specified spatial and temporal resolutions. Give feedback at: github.com/openaq/openaq-averaging
  *
  * @apiParam {string} [country] Limit results by a certain country.
  * @apiParam {string} [city] Limit results by a certain city.
  * @apiParam {string} [location] Limit results by a certain location.
- * @apiParam {string=pm25, pm10, so2, no2, o3, co, bc} [parameter] Limit to certain one or more parameters (ex. `parameter=pm25` or `parameter[]=co&parameter[]=pm25`)
+ * @apiParam {string=pm25, pm10, so2, no2, o3, co, bc} [parameter] Note: Currently only works for pm25. Limit to certain one or more parameters (ex. `parameter=pm25` or `parameter[]=co&parameter[]=pm25`)
  * @apiParam {boolean=true, false} [has_geo] Filter out items that have or do not have geographic information.
- * @apiParam {string} [coordinates] Center point (`lat, lon`) used to get measurements within a certain area. (ex. `coordinates=40.23,34.17`)
- * @apiParam {number} [radius=2500] Radius (in meters) used to get measurements within a certain area, must be used with `coordinates`.
- * @apiParam {number} [value_from] Show results above value threshold, useful in combination with `parameter`.
- * @apiParam {number} [value_to] Show results below value threshold, useful in combination with `parameter`.
- * @apiParam {string} [date_from] Show results after a certain date. This acts on the `utc` timestamp of each measurement. (ex. `2015-12-20`, or `2015-12-20T09:00:00`)
- * @apiParam {string} [date_to] Show results before a certain date. This acts on the `utc` timestamp of each measurement. (ex. `2015-12-20`, or `2015-12-20T09:00:00`)
+ * @apiParam {string} [coordinates] Center point (`lat, lon`) used to get calculations within a certain area. (ex. `coordinates=40.23,34.17`)
+ * @apiParam {number} [radius=2500] Radius (in meters) used to get calculations within a certain area, must be used with `coordinates`.
+ * @apiParam {string} [date_from] Show results after a certain date (in utc). (ex. `2015-12-20`, or `2015-12-20T09:00:00`)
+ * @apiParam {string} [date_to] Show results before a certain date (in utc). (ex. `2015-12-20`, or `2015-12-20T09:00:00`)
  * @apiParam {string[]} [order_by=date] Order by one or more fields (ex. `order_by=date` or `order_by[]=value&order_by[]=parameter`).
  * @apiParam {string[]} [sort=asc] Define sort order for one or more fields (ex. `sort=desc` or `sort[]=asc&sort[]=desc`).
- * @apiParam {array=attribution, averagingPeriod, sourceName}  [include_fields] Include extra fields in the output in addition to default values.
  * @apiParam {number} [limit=100] Change the number of results returned, max is 10000.
  * @apiParam {number} [page=1] Paginate through results. Max is set at 100.
- * @apiParam {string=csv, json} [format=json] Format for data return. Note that `csv` will return a max of 65,536 results when no limit is set.
  *
- * @apiSuccess {object}   date            Date and time of measurement in both local and UTC `default`
- * @apiSuccess {string}   parameter       Property being measured `default`
- * @apiSuccess {number}   value           Value of measurement `default`
- * @apiSuccess {string}   unit            Unit of measurement `default`
- * @apiSuccess {string}   location        Location description for measurement `default`
- * @apiSuccess {string}   country         Country containing measurement in 2 letter ISO code `default`
- * @apiSuccess {string}   city            City containing measurement `default`
- * @apiSuccess {object}   coordinates     Latitude and longitude measurement was taken at `default`
- * @apiSuccess {string}   sourceName      Name of source matching to sources table for reference
+ * @apiSuccess {string}   country         Country for calculated average in 2 letter ISO code `default`
+ * @apiSuccess {string}   city            City for calculated average
+ * @apiSuccess {string}   location        Location for calculated average
+ * @apiSuccess {object}   coordinates     Latitude and longitude for calculated average
+ * @apiSuccess {string}   parameter       Parameter for calculated average for `default`
+ * @apiSuccess {object}   date            Date and time for calculated average in UTC `default`
+ * @apiSuccess {number}   average         Calculated average `default`
+ * @apiSuccess {number}   measurement_count   Number of measurements used to calculate average `default`
+
  * @apiSuccessExample {json} Success Response:
  *      HTTP/1.1 200 OK
- *      {
- *       "parameter": "Ammonia",
- *       "date": {
- *           "utc": "2015-07-16T20:30:00.000Z",
- *           "local": "2015-07-16T18:30:00.000-02:00"
- *       },
- *       "value": "72.9",
- *       "unit": "µg/m3",
- *       "location": "Anand Vihar",
- *       "country": "IN",
- *       "city": "Delhi",
- *       "sourceName": "Anand Vihar",
- *       "averagingPeriod": {
- *          "value": 1,
- *          "unit": "hours"
- *       },
- *       "coordinates": {
- *          "latitude": 43.34,
- *          "longitude": 23.04
- *       },
- *       "attribution": [
- *         {
- *           "name" : "SINCA",
- *           "url" : "http://sinca.mma.gob.cl/"
- *         },
- *         {
- *           "name" : "Ministerio del Medio Ambiente"
- *         }
- *       ]
- *     }
+ *  {
+ *    "country": "AE",
+ *    "city": "Abu Dhabi",
+ *     "location": "US Diplomatic Post: Abu Dhabi",
+ *     "coordinates": {
+ *       "longitude": 54.43375,
+ *       "latitude": 24.4244
+ *     },
+ *     "parameter": "pm25",
+ *     "date": "2017-12-26",
+ *     "average": 41.2857,
+ *     "measurement_count": null
+ *   }
  *
  * @apiError statusCode     The error code
  * @apiError error          Error name
@@ -90,66 +67,70 @@ const defaultRequestLimit = config.get('defaultRequestLimit');
  *     }
  */
 module.exports = [
-    {
-        method: ['GET'],
-        path: '/v1/averages',
-        config: {
-            description: 'Retrieve measurement averages for specified spatial and temporal resolutions.',
-            validate: {
-                query: {
-                    city: [Joi.string(), Joi.array().items(Joi.string())],
-                    coordinates: Joi.string()
+  {
+    method: ['GET'],
+    path: '/beta/averages',
+    config: {
+      description: 'BETA - Retrieve measurement averages for specified spatial and temporal resolutions.',
+      validate: {
+        query: {
+          city: [Joi.string(), Joi.array().items(Joi.string())],
+          coordinates: Joi.string()
                         .regex(lonLatRegex)
                         .error(() => 'invalid coordinates pair'),
-                    country: [Joi.string(), Joi.array().items(Joi.string())],
-                    has_geo: Joi.boolean(),
-                    limit: Joi.number()
+          country: [Joi.string(), Joi.array().items(Joi.string())],
+                    // has_geo: Joi.boolean(),
+          limit: Joi.number()
                         .default(defaultRequestLimit)
                         .max(maxRequestLimit),
-                    location: [Joi.string(), Joi.array().items(Joi.string())],
-                    name: [Joi.string(), Joi.array().items(Joi.string())],
-                    order_by: [Joi.string(), Joi.array().items(Joi.string())],
-                    temporal: [Joi.string()], //location, city, country, global
-                    spatial: [Joi.string()], //hourly, daily (current restriction), monthly, yearly, all-time?
-                    date_from: Joi.date().iso(),
-                    date_to: Joi.date().iso(),
-                    page: Joi.number(),
-                    parameter: [Joi.string(), Joi.array().items(Joi.string())],
-                    radius: Joi.number(),
-                    sort: [
-                        Joi.string().valid('asc', 'desc'),
-                        Joi.array().items(Joi.string().valid('asc', 'desc'))
-                    ],
-                    format: Joi.string().valid('json', 'csv'),
-                    include_fields: Joi.string()
-                }
-            }
-        },
-        handler: async function (request, reply) {
-            try {
-                var params = {};
+          location: [Joi.string(), Joi.array().items(Joi.string())],
+                    // name: [Joi.string(), Joi.array().items(Joi.string())],
+          order_by: [Joi.string(), Joi.array().items(Joi.string())],
+          temporal: [Joi.string()], // day, month, year
+          spatial: [Joi.string()], // location, city, country
+          date_from: Joi.date().iso(),
+          date_to: Joi.date().iso(),
+          page: Joi.number(),
+                    // parameter: [Joi.string(), Joi.array().items(Joi.string())],
+                    // radius: Joi.number(),
+          sort: [
+            Joi.string().valid('asc', 'desc'),
+            Joi.array().items(Joi.string().valid('asc', 'desc'))
+          ]
+                    // format: Joi.string().valid('json', 'csv'),
+                    // include_fields: Joi.string()
+        }
+      }
+    },
+    handler: async function (request, reply) {
+      try {
+        var params = {};
 
                 // For GET
-                if (request.query) {
-                    params = request.query;
-                }
+        if (request.query) {
+          params = request.query;
+        }
 
                 // Set max limit based on env var or default to 10000
-                request.limit = Math.min(
+        request.limit = Math.min(
                     request.limit,
                     process.env.REQUEST_LIMIT || 10000
                 );
 
                 // 6/20/2019 We're seeing major database issues related to someone trying
                 // to query the API for all the data in the system and making large page requests.
-                if (request.page > (process.env.REQUEST_PAGE || 100)) {
-                    return reply(Boom.badRequest('page limit is set too high'));
-                }
+        if (request.page > (process.env.REQUEST_PAGE || 100)) {
+          return reply(Boom.badRequest('page limit is set too high'));
+        }
+
+                // Set default spatial and temporal to location and day
+        if (params.spatial === '') { params.spatial = 'location'; }
+        if (params.temporal === '') { params.temporal = 'day'; }
 
                 // Check if this is supposed to be formatted as csv
-                var formatForCSV = false;
-                if (params.format === 'csv') {
-                    formatForCSV = true;
+        var formatForCSV = false;
+        if (params.format === 'csv') {
+          formatForCSV = true;
 
                     // Set a different max limit for CSVs to get a lot of data, but not
                     // all of it, because that's just crazy. Setting to 65,536 since this
@@ -157,30 +138,30 @@ module.exports = [
 
                     // This gets a bit weird since we want to honor any orig set limit,
                     // regardless of what was just set above
-                    let limit = request.url.query.limit || 65536;
-                    request.limit = Math.min(limit, 65536);
+          let limit = request.url.query.limit || 65536;
+          request.limit = Math.min(limit, 65536);
 
                     // Force to include attribution, handle case where it may have already
                     // been present.
-                    if (params.include_fields === undefined) {
-                        params.include_fields = 'attribution';
-                    } else {
-                        if (params.include_fields.indexOf('attribution') === -1) {
-                            params.include_fields += ',attribution';
-                        }
-                    }
-                }
-                params = _.omit(params, 'format');
+          if (params.include_fields === undefined) {
+            params.include_fields = 'attribution';
+          } else {
+            if (params.include_fields.indexOf('attribution') === -1) {
+              params.include_fields += ',attribution';
+            }
+          }
+        }
+        params = _.omit(params, 'format');
 
                 // Run query
-                let { records, count } = await m.query(
+        let { records, count } = await m.query(
                     params,
                     request.page,
                     request.limit
                 );
 
-                if (formatForCSV) { //IGNORE FOR NOW
-                    console.log("csv option")
+        if (formatForCSV) { // IGNORE FOR NOW
+        //   console.log('csv option');
                     // var columns = [
                     //     'location',
                     //     'city',
@@ -222,13 +203,13 @@ module.exports = [
                     //         'attachment;filename=openaq.csv'
                     //     );
                     // });
-                } else {
-                    request.count = count;
-                    return reply(records);
-                }
-            } catch (err) {
-                return reply(Boom.badImplementation(err));
-            }
+        } else {
+          request.count = count;
+          return reply(records);
         }
+      } catch (err) {
+        return reply(Boom.badImplementation(err));
+      }
     }
+  }
 ];
